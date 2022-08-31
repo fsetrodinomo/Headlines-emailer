dd4c5c7e56269a044a9a5b1bd609b58cdc1309e0
Fauve <fsetrodinomo@protonmail.com>
2022-08-31 14:34:55 +0200

    deciding which element should be extracted

--- headline.py
+++ headline.py
@@ -14,11 +14,23 @@ import datetime
 now = datetime.datetime.now()
 
 #email content placeholder
+#global object
 content = ''
 
 def extract_news(url):
     print ('Extracting financial times news stories ... ')
     cnt = ''
     cnt +=('<b>Financial Times Top Stories :</b>\n'+'<br>'+'-'*50+'<br>')
-   #get content of the url 
+   #get content of the url and store in response object
     response = requests.get(url)
+    #local object only for extracting news
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    for i ,tag in enumerate(soup.find_all('div',attrs={'class':'o-teaser__content','o-teaser__meta':''})):
+        cnt += ((str(i+1)+' :: '+tag.text+ "\n" + '<br>') if tag.text!='More' else'')
+    return (cnt)
+
+    cnt = extract_news('https://news.ycombinator.com/')
+    content += cnt
+    content += ('<br>-------<br>')
+    content += ('<br><br>End of message')
