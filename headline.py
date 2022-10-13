131f604f2b9a51eda35e31e7df91351e4b905218
Fauve <61892539+fsetrodinomo@users.noreply.github.com>
2022-10-13 14:54:22 +0200

    scrape script works
    email not neccesary for this project, maybe future feature

--- headline.py
+++ headline.py
@@ -3,24 +3,24 @@ import requests
 
 #webscraping
 from bs4 import BeautifulSoup
-#package to send the email
-import smtplib
-#creating email body
-from email.mime.multipart import  MIMEMultipart
-from email.mime.text import MIMEText
-#system date and time manipulation
-import datetime
-#create a seperate line with the date time to make sure that it is seperating automated email
-now = datetime.datetime.now()
+# #package to send the email
+# import smtplib
+# #creating email body
+# from email.mime.multipart import  MIMEMultipart
+# from email.mime.text import MIMEText
+# #system date and time manipulation
+# import datetime
+# #create a seperate line with the date time to make sure that it is seperating automated email
+# now = datetime.datetime.now()
 
 #email content placeholder
 #global object
 content = ''
 
 def extract_news(url):
-    print ('Extracting financial times news stories ... ')
+    print ('New jobs... ')
     cnt = ''
-    cnt +=('<b>Financial Times Top Stories :</b>\n'+'<br>'+'-'*50+'<br>')
+    cnt +=('<b>Our newest jobs :</b>\n'+'<br>'+'-'*50+'<br>')
    #get content of the url and store in response object
    
 URL = "https://realpython.github.io/fake-jobs/"
@@ -59,36 +59,36 @@ for job_element in python_job_elements :
 
 
 
-##Sending email
+# ##Sending email
 
-print ('Composing email ...')
+# print ('Composing email ...')
 
-#update email adress
-SERVER = 'smtp.office365.com'
-PORT = 587
-FROM = "jolieaurelius@outlook.com"
-TO = "jolieaurelius@outlook.com"
-PASS = "Duckduck123"
+# #update email adress
+# SERVER = 'smtp.office365.com'
+# PORT = 587
+# FROM = ""
+# TO = ""
+# PASS = ""
 
-#message body
-msg = MIMEMultipart ()
+# #message body
+# msg = MIMEMultipart ()
 
-msg['Subject']  = 'Financial times headlines ' + '' +str(now.day) + '-' + str(now.month) + '-' + str(
-    now.year) 
-msg ['From'] = FROM
-msg ['To'] = TO
+# msg['Subject']  = 'Financial times headlines ' + '' +str(now.day) + '-' + str(now.month) + '-' + str(
+#     now.year) 
+# msg ['From'] = FROM
+# msg ['To'] = TO
 
-msg.attach(MIMEText(content,'html'))
+# msg.attach(MIMEText(content,'html'))
 
-print ('Ínitiating server ......')
+# print ('Ínitiating server ......')
 
-server = smtplib.SMTP(SERVER, PORT)
-server.set_debuglevel(1)
-server.ehlo()
-server.starttls()
-server.login(FROM, PASS)
-server.sendmail(FROM,TO, msg.as_string())
+# server = smtplib.SMTP(SERVER, PORT)
+# server.set_debuglevel(1)
+# server.ehlo()
+# server.starttls()
+# server.login(FROM, PASS)
+# server.sendmail(FROM,TO, msg.as_string())
 
-print('Email sent ....')
+# print('Email sent ....')
 
-server.quit()
+# server.quit()
